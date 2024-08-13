f34afcfefa04c2741423ba52a2ea1569501d557b
arieljchase <200566592@student.georgianc.on.ca>
2024-08-13 17:06:41 -0400

    question 6 complete + fixes for customer class

--- src/main/java/com/example/comp1011finalexamsummer2024/Customer.java
+++ src/main/java/com/example/comp1011finalexamsummer2024/Customer.java
@@ -1,25 +1,77 @@
-//Ariel Chase - 200566592
-//Must track (at minimum):
-//id, first name, last name, phone and purchased products
-
 package com.example.comp1011finalexamsummer2024;
 
 import com.google.gson.JsonArray;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.List;
 
 public class Customer {
-    int id;
-    public String firstName;
-    public String lastName;
-    public String phoneNumber;
-    private JsonArray jsonObject;
-    JsonArray purchases = jsonObject;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private List<String> purchasedProducts;
 
-    List<JsonArray> totalPurchases = Arrays.asList(purchases);
+    // Constructor
+    public Customer(int id, String firstName, String lastName, String phoneNumber, List<String> purchasedProducts) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.purchasedProducts = purchasedProducts;
+    }
 
+    // Getters and Setters - Need for TableView
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<String> getPurchasedProducts() {
+        return purchasedProducts;
+    }
+
+    public void setPurchasedProducts(List<String> purchasedProducts) {
+        this.purchasedProducts = purchasedProducts;
+    }
+
+    // Calculate total purchases
+    public double getTotalPurchases() {
+
+        double total = 0.0;
+        for (String product : purchasedProducts) {
+            total += parsePriceFromProduct(product);
+        }
+        return total;
+    }
+
+    private double parsePriceFromProduct(String product) {
+        return 0.0;
+    }
 }
-
-
--- src/main/java/com/example/comp1011finalexamsummer2024/HelloApplication.java
+++ src/main/java/com/example/comp1011finalexamsummer2024/HelloApplication.java
@@ -5,6 +5,9 @@ package com.example.comp1011finalexamsummer2024;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,14 +15,32 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("table-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        //Question 6: Creating our TableView
+        TableView<Customer> table = new TableView<>();
+
+        TableColumn<Customer, Integer> idCol = new TableColumn<>("Id");
+        TableColumn<Customer, String> firstnameCol = new TableColumn<>("First Name");
+        TableColumn<Customer, String> lastnameCol = new TableColumn<>("Last Name");
+        TableColumn<Customer, String> phoneCol = new TableColumn<>("Phone Number");
+        TableColumn<Customer, String> purchaseCol = new TableColumn<>("Total Purchases");
+
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        firstnameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lastnameCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        phoneCol.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
+        purchaseCol.setCellValueFactory(new PropertyValueFactory<>("totalPurchases"));
+
+        table.getColumns().addAll(idCol, firstnameCol, lastnameCol, phoneCol, purchaseCol);
     }
 
     public static void main(String[] args) {
         launch();
     }
 }
+
